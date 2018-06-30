85001469a2de72c3e803f2d22b64e75170d90378
Hoan PP <hoanbk02@gmail.com>
2018-07-01 00:10:20 +0700

    delete hello

--- src/internetbanking/InternetBanking.java
+++ src/internetbanking/InternetBanking.java
@@ -16,7 +16,6 @@ public class InternetBanking {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello world");
     }
     
 }
